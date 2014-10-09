ebd575cfd9d9a91c2936839409647bf36cb03fb8
Russell Phillippe <russell@protogenos.co.uk>
2014-10-09 22:05:06 +0100

    no message

--- scraper.py
+++ scraper.py
@@ -5,14 +5,14 @@ import scraperwiki
 import lxml.html
 
 # Read in a page
-html = scraperwiki.scrape("https://api.twitter.com/1.1/search/tweets.json")
+html = scraperwiki.scrape("http://www.yorkcocoahouse.co.uk")
 
 # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-root.cssselect("div[align='left']")
+root.cssselect("div[class='level1 nav-7-2']")
 
 # Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['name'], data=html)
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 
 # An arbitrary query against the database
 scraperwiki.sql.select("* from data where 'name'='peter'")
