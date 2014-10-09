1df09a1534d3f4752496d9c61da3432e207bf555
Russell Phillippe <russell@protogenos.co.uk>
2014-10-09 22:31:31 +0100

    no message

--- scraper.py
+++ scraper.py
@@ -1,65 +1,64 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+DATABASE_NAME = 'data.sqlite'
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import os
+os.environ['SCRAPERWIKI_DATABASE_NAME'] = DATABASE_NAME
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
 import scraperwiki
 import lxml.html
-import datetime
-import time
-import re
+from pprint import pprint
 
-from threading import Thread
-url = ['http://www.indiegogo.com/projects?filter_category=Technology&filter_country=CTRY_US&filter_quick=popular_all&pg_num=']
-for x in url:
-    def indiegogo():
-        for i in range(1,4):
-            try:
-                url = x+str(i)
-                root = scraperwiki.scrape(url)
-                content = lxml.html.etree.HTML(root)
-                categorie = content.xpath('//div[contains(@class,"project-category")]')
-                nom = content.xpath('//div[contains(@class,"project-details")]/a')
-                #porteur_projet = content.xpath('//p[contains(@class,"creator")]/a')
-                porteur_projet = content.xpath('//p[contains(@class,"creator")]/span/a')
-                details = content.xpath('//p[contains(@class,"description")]')
-                stats = content.xpath('//span[contains(@id,"project-stats-funding-pct")]')
-                montant = content.xpath('//span[contains(@class,"currency currency-medium")]/span')
-                lien = content.xpath('//div[contains(@class,"project-details")]/a[@href]')
-                for karazana,projet,olona,deta,marika,vola,rohy in zip(categorie,nom,porteur_projet,details,stats,montant,lien):
-            #print olona.text,asa.text,deta.text,marika.text,vola.text
-                    data = {
-                                'Categorie': karazana.text,
-                                'Projet' : projet.text,
-                                'Nom du porteur': olona.text,
-                                'details': deta.text,
-                                'statistique': marika.text,
-                                'montant obtenu': vola.text,
-                                'lien':  "http://www.indiegogo.com"+rohy.attrib.get('href'),
-                                'date' : datetime.datetime.now()
-                            }
-                    scraperwiki.sqlite.save(unique_keys=['Projet'], data=data)
+result_schema = {
+        'rank': None,
+        'bib': None,
+        'fis_code': None,
+        'name': None,
+        'year_of_birth': None,
+        'nation': None,
+        'run_1': None,
+        'run_2': None,
+        'total_time': None,
+        'diff': None,
+        'fis_points': None,
+}
 
+# generator of FIS race links
+def race_link_results(url):
+    html = scraperwiki.scrape(url)
+    root = lxml.html.fromstring(html)
+    result_table = root.cssselect("table.fisfootable")[0]
+    status_cells = result_table.cssselect("td.status")
+    for status_cell in status_cells:
+        result_div = status_cell.cssselect("div")[1]
+        for element, attribute, link, pos in result_div.iterlinks():
+            event_page = scraperwiki.scrape(link)
+            event_root = lxml.html.fromstring(event_page)
+            event_rows = event_root.cssselect("table.footable tbody tr")
+            for event_row in event_rows:
+                last_cell = event_row.cssselect("td:last-child")[0]
+                for race_links in last_cell.iterlinks():
+                    extra = {
+                        'date': get_cell_value(event_row.cssselect("td")[1], "span a"),
+                        'place': get_cell_value(event_row.cssselect("td")[2], "span a"),
+                        'country': get_cell_value(event_row.cssselect("td")[3], "a span"),
+                        'codex': get_cell_value(event_row.cssselect("td")[4], "a"),
+                        'discipline': get_cell_value(event_row.cssselect("td")[5], "a"),
+                    }
+                    yield (race_links[2], extra)
 
-            except:
-                print "None"
-    t = Thread(target=indiegogo)
-    t.start()
+def get_cell_value(element, css):
+    return element.cssselect(css)[0].text_content()
+
+FIS_URL = "http://data.fis-ski.com/alpine-skiing/results.html"
+for link, raceinfo in race_link_results(FIS_URL):
+    # html = scraperwiki.scrape(link)
+    # root = lxml.html.fromstring(html)
+    # result_table = root.cssselect("table.fisfootable")[0]
+    print link
+    print raceinfo['date']
+
+# # Write out to the sqlite database using scraperwiki library
+# data = {"name": "susan", "occupation": "software developer"}
+# scraperwiki.sqlite.save(unique_keys=['name'], data=data, table_name="data")
+#
+# An arbitrary query against the database
+# pprint(scraperwiki.sql.select("* from data where 'name'='susan'"))
